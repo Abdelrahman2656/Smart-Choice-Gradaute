cbfb785b22e12fb0fb0fbb4fcfa4ee65587c3a3d
Abdelrahman2656 <a.elmonged870@gmail.com>
2025-03-09 01:14:34 +0200

    update

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const bootstrap_1 = require("./Src/bootstrap");
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 //bootstrap
 (0, bootstrap_1.bootstrap)(app, express);
 exports.default = app;
 app.get('/', (req, res) => res.send('Hello World! g'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- index.ts
+++ index.ts
@@ -3,9 +3,10 @@ import { bootstrap } from "./Src/bootstrap"
 import { AppRequest, AppResponse } from "./Src/Utils/type"
 const express = require('express')
 const app = express()
-const port = process.env.PORT||3001
+const port = process.env.PORT||3000
 
 //bootstrap
 bootstrap(app, express)
 export default app
 app.get('/', (req:AppRequest, res:AppResponse) => res.send('Hello World! g'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
